306ce17b6050699635850546bffd70854fd9d33a
Your Name <you@example.com>
2025-07-25 20:24:59 +0300

    Major changes, newer CLI structure

--- src/main.zig
+++ src/main.zig
@@ -28,14 +28,17 @@ pub fn main() !void {
 
     const f = flags.parseOrExit(args, "adum", Flags, .{});
 
-    var p = try zmaps.PM.init(f.pid);
-    defer p.deinit();
+    if (f.command.search.target_module_name) |target_mod| {
+        var p = try zmaps.PM.init(f.pid);
+        defer p.deinit();
 
-    const map = try p.findModule(f.module_name);
-
-    std.log.debug("{?s} at \x1b[0;36m{x}-{x}\x1b[0;37m\n", .{ map.path, map.start, map.end });
-    if (f.dump_path) |path| {
-        try Dumper.dumpMemoryToFile(f.pid, map.start, map.end, path);
+        const map = try p.findModule(target_mod);
+        std.log.debug("{?s} at \x1b[0;36m{x}-{x}\x1b[0;97m\n", .{ map.path, map.start, map.end });
+        if (f.command.search.dump) {
+            if (f.command.search.dump_out_path) |path| {
+                try Dumper.dumpMemoryToFile(f.pid, map.start, map.end, path);
+            } else return error.NoDumpOutputPathSpecified;
+        }
     }
 }
 
@@ -45,12 +48,26 @@ const Flags = struct {
     ;
 
     pid: ?i32,
-    module_name: []const u8,
-    dump_path: ?[]const u8,
+
+    command: union(enum) {
+        search: struct {
+            dump: bool,
+            target_module_name: ?[]const u8,
+            dump_out_path: ?[]const u8,
+
+            pub const switches = .{
+                .dump = 'd',
+                .target_module_name = 't',
+                .dump_out_path = 'o',
+            };
+        },
+
+        pub const descriptions = .{
+            .search = "Search for a module in memory and optionally dump.",
+        };
+    },
 
     pub const switches = .{
         .pid = 'p',
-        .module_name = 'm',
-        .dump_path = 'o',
     };
 };
